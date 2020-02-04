ed197c68004bd993b5669725b62b4cc190524852
NostalgiaBox <rfenster@gmail.com>
2020-02-03 21:59:46 -0700

    Added some basic controls to do a hello world site

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello, Javascript Developer</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('The server is up and running on port 3000')
+});
