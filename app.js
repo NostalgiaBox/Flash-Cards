65ea8deee7885656ab09724396a777a07ed309fd
NostalgiaBox <rfenster@gmail.com>
2020-02-04 20:36:59 -0700

    Added in pug templates and Pug framework

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.render('index');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello, Javascript Developer</h1>');
+app.get('/cards', (req, res) => {
+    res.render('card', {prompt: "Who is buried in Grant's Tomb?", hint: "Think about who's tomb it is..."});
 });
 
 app.listen(3000, () => {
